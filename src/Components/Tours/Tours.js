51935a7b850e10e57fd4d3df8edf2df9f2055597
Brock Mortenson <mortensonbrock@gmail.com>
2021-04-15 12:30:41 -0600

    Finished styling on the Home and Tours components (Home page)

--- src/Components/Tours/Tours.js
+++ src/Components/Tours/Tours.js
@@ -19,8 +19,9 @@ const Tours = () => {
             <div className='tour-container'>
                 <div className='individual-tour'>
                     <div className='tour-title'>{tour.title}</div>
-                    <div className='tour-price'>{tour.price}</div>
+                    <div className='tour-price'>${tour.price}</div>
                     <div className='tour-summary'>{tour.summary}</div>
+                    <button className='tour-btn'>Book Now</button>
                 </div>
             </div>
         )
@@ -32,8 +33,11 @@ const Tours = () => {
             <h2 className='tours-list'>{mappedTours}</h2>
             <div className='tour-images'>
                 <img className='basic-paris' src='https://res.allmacwallpaper.com/pic/Thumbnails/2558_728.jpg' alt='basic-paris' />
-                <img className='full-paris' src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTrSaQJtUoaVDpxKRb5z9rH76gqE26v9hQitw&usqp=CAU' alt='full-paris' />
-                <img className='inclusive-paris' />
+                <img className='full-paris' src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ9IszB6YE-aDE8YtUGGvVUnZ6e-lPsir5xaA&usqp=CAU' alt='full-paris' />
+                <img className='inclusive-paris' src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTrSaQJtUoaVDpxKRb5z9rH76gqE26v9hQitw&usqp=CAU' alt='inclusive-paris' />
+                <img className='basic-italy' src='https://wallpaperaccess.com/full/115157.jpg' alt='basic=italy' />
+                <img className='full-italy' src='https://s1.1zoom.me/b5346/654/Bridges_Rivers_Evening_Italy_Rome_Cathedral_550636_1920x1080.jpg' alt='full-italy' />
+                <img className='inclusive-italy' src='https://wallpaperaccess.com/full/115111.jpg' alt='inclusive-italy' />
             </div>
         </div>
     );
