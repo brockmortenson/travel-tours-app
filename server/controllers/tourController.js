5d0a134b7b7861d7e913c8323362b95dbbbfff73
Brock Mortenson <mortensonbrock@gmail.com>
2021-04-22 09:28:26 -0600

    hosting commit attempt4

--- server/controllers/tourController.js
+++ server/controllers/tourController.js
@@ -7,5 +7,8 @@ module.exports = {
         console.log(req.body)
         const addedTours = await req.app.get('db').add_cart_item_to_cart(cart_id, tour_id);
         return res.status (200).send(addedTours);
-    }
+    },
+    // remove
 }
+
+// comment
